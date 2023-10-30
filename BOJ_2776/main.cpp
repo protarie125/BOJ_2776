f0fc1081426460d7eb7e401395a9ba6b579c8201
protarie125 <protarie@gmail.com>
2023-10-30 16:13:27 +0900

    초기 커밋.

---
+++ BOJ_2776/main.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <bit>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+using vs = vector<string>;
+
+int n;
+vi dict;
+int m;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+	while (0 < (t--)) {
+		cin >> n;
+		dict = vi(n);
+		for (auto&& x : dict) {
+			cin >> x;
+		}
+		sort(dict.begin(), dict.end());
+
+		cin >> m;
+		while (0 < (m--)) {
+			int x;
+			cin >> x;
+
+			if (std::binary_search(dict.begin(), dict.end(), x)) {
+				cout << 1 << '\n';
+			}
+			else {
+				cout << 0 << '\n';
+			}
+		}
+	}
+
+	return 0;
+}
